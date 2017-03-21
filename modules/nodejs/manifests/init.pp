a0c11621b3219d0e2d5e7d590de6818b4824fff2
Stephen Edgar <stephen@netweb.com.au>
2017-03-21 18:03:59 +1100

    Use single quotes where required

--- modules/nodejs/manifests/init.pp
+++ modules/nodejs/manifests/init.pp
@@ -1,11 +1,11 @@
 class nodejs (
-  $path = "/vagrant/extensions/nodejs"
+  $path = '/vagrant/extensions/nodejs'
 ) {
   if $lsbdistcodename == 'xenial' {
     $clang_package = '3.8'
   }
   else {
-    $clang_package = "3.4"
+    $clang_package = '3.4'
   }
   package { "clang-${$clang_package}":
     ensure  => latest,
@@ -15,7 +15,7 @@ class nodejs (
     require => Exec['node update']
   }
   exec { 'node update':
-    command => "/usr/bin/curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -",
+    command => '/usr/bin/curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -',
     require => Package['curl']
   }
 }
