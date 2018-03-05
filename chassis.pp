a13f2150ed1deecf65f324ca9f5711cc523f717f
Bronson Quick <bronson@bronsonquick.com.au>
2018-03-05 14:22:29 +1000

    Start adding in disabled_extensions support

--- chassis.pp
+++ chassis.pp
@@ -1,3 +1,4 @@
+$nodejs_config = sz_load_config()
 class { 'nodejs':
   path => '/vagrant/extensions/nodejs'
 }
--- modules/nodejs/manifests/init.pp
+++ modules/nodejs/manifests/init.pp
@@ -1,22 +1,35 @@
 # Install NodeJS
 class nodejs (
-  $path = '/vagrant/extensions/nodejs'
+	$path = '/vagrant/extensions/nodejs'
 ) {
-  if $::lsbdistcodename == 'xenial' {
-    $clang_package = '3.8'
-  }
-  else {
-    $clang_package = '3.4'
-  }
-  package { "clang-${$clang_package}":
-    ensure => latest,
-  }
-  package { 'nodejs':
-    ensure  => latest,
-    require => Exec['node update']
-  }
-  exec { 'node update':
-    command => '/usr/bin/curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -',
-    require => Package['curl']
-  }
+	if ( ! empty($::nodejs_config[disabled_extensions]) and 'xdebug' in $::nodejs_config[disabled_extensions] ) {
+		$package = absent
+		$file = absent
+	} else {
+		$package = latest
+		$file = 'present'
+	}
+	if $::lsbdistcodename == 'xenial' {
+		$clang_package = '3.8'
+	}
+	else {
+		$clang_package = '3.4'
+	}
+	package { "clang-${$clang_package}":
+		ensure => $package,
+	}
+	if ( 'latest' == $package ) {
+		exec { 'node update':
+			path    => [ '/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/' ],
+			command => '/usr/bin/curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -',
+			require => Package['curl'],
+			unless  => 'which node'
+		}
+		package { 'nodejs':
+			ensure  => $package,
+			require => Exec['node update']
+		}
+	} else {
+		notice("Uninstall node and npm")
+	}
 }
